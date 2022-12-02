7ef48b4ae3eff29b23a71e271a0d04dbf6e1537c
Gunner <hopkinsgd@hendrix.edu>
2022-12-01 21:17:00 -0600

    Button Formatting pt. 1

--- lib/suggestions.dart
+++ lib/suggestions.dart
@@ -18,61 +18,77 @@ class WorkoutSuggestions extends StatelessWidget {
                   Navigator.pop(context);
                 } , 
                 child: const Text("Return")),
-              TextButton(
-                  onPressed: () {
-                    showDialog(
-                        context: context,
-                        builder: (BuildContext context) {
-                          return AlertDialog(
-                            title: Text("Legs"),
-                            content: Text(
-                                "BB Back Squat - 4 x 6, 75% Max, Front Squat - 4 x 4, 75% Max, Leg Curls - 4 x 10, 85% Max",
-                                style: textstyle2 ),
-                          );
-                        });
-                  },
-                  child: const Text("Legs")),
-              TextButton(
-                  onPressed: () {
-                    showDialog(
-                        context: context,
-                        builder: (BuildContext context) {
-                          return AlertDialog(
-                            title: Text("Arms"),
-                            content: Text(
-                                "Bicep Curls - 4 x 10, Tricep Extentions - 4 x 10, Preacher Curls 3 x 10, Dips - 3 x 10",
-                                style: textstyle2 ),
-                          );
-                        });
-                  },
-                  child: const Text("Arms")),
-              TextButton(
-                  onPressed: () {
-                    showDialog(
-                        context: context,
-                        builder: (BuildContext context) {
-                          return AlertDialog(
-                            title: Text("Chest"),
-                            content: Text(
-                                "BB Bench Press - 4 x 6, DB Bench Press - 4 x 6, DB Incline Bench - 4 x 8, Push ups - 3 x 10 ",
-                                style: textstyle2 ),
-                          );
-                        });
-                  },
-                  child: const Text("Chest")),
-              TextButton(
-                  onPressed: () {
-                    showDialog(
-                        context: context,
-                        builder: (BuildContext context) {
-                          return AlertDialog(
-                            title: Text("Back"),
-                            content: Text(
-                                "BB Row - 4 x 8, DB Row - 4 x 8, Face Pulls - 4 x 10, Pull ups - 3 x 10",
-                                style: textstyle2 ),
-                          );
-                        });
-                  },
-                  child: const Text("Back"))]))));
+              Container(
+                width: 300,
+                height: 50,
+                child: TextButton(
+                    onPressed: () {
+                      showDialog(
+                          context: context,
+                          builder: (BuildContext context) {
+                            return AlertDialog(
+                              title: Text("Legs"),
+                              content: Text(
+                                  "BB Back Squat - 4 x 6, 75% Max, Front Squat - 4 x 4, 75% Max, Leg Curls - 4 x 10, 85% Max",
+                                  style: textstyle2 ),
+                            );
+                          });
+                    },
+                    child: const Text("Legs")),
+              ),
+              Container(
+                width: 300,
+                height: 50,
+                child: TextButton(
+                    onPressed: () {
+                      showDialog(
+                          context: context,
+                          builder: (BuildContext context) {
+                            return AlertDialog(
+                              title: Text("Arms"),
+                              content: Text(
+                                  "Bicep Curls - 4 x 10, Tricep Extentions - 4 x 10, Preacher Curls 3 x 10, Dips - 3 x 10",
+                                  style: textstyle2 ),
+                            );
+                          });
+                    },
+                    child: const Text("Arms")),
+              ),
+              Container(
+                width: 300,
+                height: 50,
+                child: TextButton(
+                    onPressed: () {
+                      showDialog(
+                          context: context,
+                          builder: (BuildContext context) {
+                            return AlertDialog(
+                              title: Text("Chest"),
+                              content: Text(
+                                  "BB Bench Press - 4 x 6, DB Bench Press - 4 x 6, DB Incline Bench - 4 x 8, Push ups - 3 x 10 ",
+                                  style: textstyle2 ),
+                            );
+                          });
+                    },
+                    child: const Text("Chest")),
+              ),
+              Container(
+                width: 300,
+                height: 50,
+                child: TextButton(
+                    onPressed: () {
+                      showDialog(
+                          context: context,
+                          builder: (BuildContext context) {
+                            return AlertDialog(
+                              title: Text("Back"),
+                              content: Text(
+                                  "BB Row - 4 x 8, DB Row - 4 x 8, Face Pulls - 4 x 10, Pull ups - 3 x 10",
+                                  style: textstyle2 ),
+                            );
+                          });
+                    },
+                    child: const Text("Back")),
+              )]))));
   }
 }
