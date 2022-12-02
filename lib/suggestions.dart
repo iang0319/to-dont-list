078668d31016c19290929683b14429e3d3930d58
Gunner <hopkinsgd@hendrix.edu>
2022-12-01 19:51:59 -0600

    Added widget for legs suggestions

--- lib/suggestions.dart
+++ lib/suggestions.dart
@@ -1,2 +1,31 @@
 import 'package:flutter/material.dart';
 
+class WorkoutSuggestions extends StatelessWidget {
+  const WorkoutSuggestions({super.key});
+
+  final standard = "";
+  final TextStyle textstyle2 = const TextStyle(fontSize: 15, fontStyle: FontStyle.italic);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: SingleChildScrollView(
+          child: Column(
+            children: <Widget>[
+              TextButton(
+                  onPressed: () {
+                    showDialog(
+                        context: context,
+                        builder: (BuildContext context) {
+                          return AlertDialog(
+                            title: Text("Legs"),
+                            content: Text(
+                                "BB Back Squat - 4 x 6, 75% Max, Front Squat - 4 x 4, 75% Max, Leg Curls - 4 x 10, 85% Max",
+                                style: textstyle2 ),
+                          );
+                        });
+                  },
+                  child: const Text("pH"))]))));
+  }
+}
